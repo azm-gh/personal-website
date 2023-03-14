0b3b2133352b17068c540e0cad8b2b65fef891d1
Jakub Jehlík <jakubjehlik@Jakubs-MacBook-Air.local>
2023-03-14 15:00:09 +0100

    profile changes

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ export function Profile() {
 
     return(
         <section className="relative mx-auto bg-gradient-to-b from-teal-501 rounded-full w-80 h-80 mt-20 overflow-hidden">
-              <Image src="/profile.png" width={330} height={200} alt="me" />
+              <Image src="/jjj.jpeg" width={330} height={200} alt="me" />
         </section>
     ) 
 }
